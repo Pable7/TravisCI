0d31a78f1edc9a447b0db41d3feca28b6a082c96
Pablo Villegas <pvillegas241@gmail.com>
2019-05-19 12:11:17 -0500

    Datos de granjas agregados

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import Dependencies.matlib as matlib
+import csv
 
-a  = matlib.division(4, 0)
+with open('./Data/nacimientos.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["granja"]} had {row["partos"]} parts, and were born {row["nacidos_vivos"]} pigs.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
